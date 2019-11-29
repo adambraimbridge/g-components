d3393147d5b9a314a3807404cfbb1256d292eac2
Ændrew Rininsland <andrew.rininsland@ft.com>
2019-11-29 16:35:51 +0000

    Adds expander to ConstituencyResultsTable; blocked by design

--- components/elections/constituency-results-table/index.js
+++ components/elections/constituency-results-table/index.js
@@ -8,18 +8,50 @@ import PropTypes from 'prop-types';
 import { uk } from '@financial-times/politics';
 import { Context } from '../../layout';
 import { numberWithCommas } from '../utils';
+import { Expander } from '../../expander';
 import './styles.scss';
 
 const { getPartyInfo } = uk;
 
-export const ConstituencyResultsTable = ({ className, data, tableHeaders, note, sortFunction }) => {
-  const { breakpoint } = useContext(Context);
+const ConstituencyResultsTableRow = ({ party, candidate, votes, showAsterick }) => {
+  const { shortName, color } = getPartyInfo(party);
+  return (
+    <tr key={`row_${party}`}>
+      <td className="party">
+        <span className="party-badge" style={{ backgroundColor: color }} />
+        <span className="party-name">{shortName}</span>
+      </td>
+      <td className="candidate-name">
+        {candidate}
+        {showAsterick && '*'}
+      </td>
+      {votes && <td className="number">{numberWithCommas(votes)}</td>}
+    </tr>
+  );
+};
+
+export const ConstituencyResultsTable = ({
+  className,
+  data,
+  tableHeaders,
+  note,
+  sortFunction,
+  expander: showExpander,
+}) => {
+  const { breakpoint = 'default' } = useContext(Context);
   const sortedData = sortFunction
     ? data.sort((a, b) => sortFunction(a, b))
     : data.sort((a, b) =>
         a.votes && b.votes ? b.votes - a.votes : a.candidate.localeCompare(b.candidate),
       );
-
+  const rows = sortedData.map(({ party, candidate, votes, showAsterick }) => (
+    <ConstituencyResultsTableRow
+      party={party}
+      candidate={candidate}
+      votes={votes}
+      showAsterick={showAsterick}
+    />
+  ));
   return (
     <div className={className}>
       <table className={`${className}__table`}>
@@ -31,22 +63,11 @@ export const ConstituencyResultsTable = ({ className, data, tableHeaders, note, 
           </tr>
         </thead>
         <tbody>
-          {sortedData.map(({ party, candidate, votes, showAsterick }) => {
-            const { shortName, color } = getPartyInfo(party);
-            return (
-              <tr key={`row_${party}`}>
-                <td className="party">
-                  <span className="party-badge" style={{ backgroundColor: color }} />
-                  <span className="party-name">{shortName}</span>
-                </td>
-                <td className="candidate-name">
-                  {candidate}
-                  {showAsterick && '*'}
-                </td>
-                {votes && <td className="number">{numberWithCommas(votes)}</td>}
-              </tr>
-            );
-          })}
+          {['l', 'xl'].includes(breakpoint.toLowerCase()) || showExpander ? (
+            <Expander>{rows}</Expander>
+          ) : (
+            rows
+          )}
         </tbody>
       </table>
       {note && <div className={`${className}__note`}>{note}</div>}
@@ -67,10 +88,12 @@ ConstituencyResultsTable.propTypes = {
   tableHeaders: PropTypes.arrayOf(PropTypes.string).isRequired,
   note: PropTypes.string,
   sortFunction: PropTypes.func,
+  expander: PropTypes.bool,
 };
 
 ConstituencyResultsTable.defaultProps = {
   className: 'g-constituency-results-table',
+  expander: false,
 };
 
 export default ConstituencyResultsTable;
