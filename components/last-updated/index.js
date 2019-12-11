565f72c0ff15af1d94decde5e5e7114672e9b5bf
Ændrew Rininsland <andrew.rininsland@ft.com>
2019-12-11 01:50:38 +0000

    Hides last updated if passed a false-y value.

--- components/last-updated/index.js
+++ components/last-updated/index.js
@@ -8,34 +8,35 @@ import PropTypes from 'prop-types';
 import DateTime from '../datetime';
 import './styles.scss';
 
-const LastUpdated = ({ lastUpdated, live }) => (
-  <div className={`last-updated${live ? ' last-updated--live' : ''}`}>
-    {live ? (
-      <div className="o-teaser o-teaser--small" data-o-component="o-teaser">
-        <div className="o-teaser__content">
-          <div
-            className="o-teaser__timestamp o-teaser__timestamp--inprogress"
-            style={{ display: 'inline-block' }}
-          >
-            <span className="o-teaser__timestamp-prefix" />
+const LastUpdated = ({ lastUpdated, live }) =>
+  lastUpdated && lastUpdated.instanceOf(Date) ? (
+    <div className={`last-updated${live ? ' last-updated--live' : ''}`}>
+      {live ? (
+        <div className="o-teaser o-teaser--small" data-o-component="o-teaser">
+          <div className="o-teaser__content">
+            <div
+              className="o-teaser__timestamp o-teaser__timestamp--inprogress"
+              style={{ display: 'inline-block' }}
+            >
+              <span className="o-teaser__timestamp-prefix" />
+            </div>
+            Last updated <DateTime datestamp={lastUpdated} />
           </div>
-          Last updated <DateTime datestamp={lastUpdated} />
         </div>
-      </div>
-    ) : (
-      <span>
-        Last updated <DateTime datestamp={lastUpdated} />
-      </span>
-    )}
-  </div>
-);
+      ) : (
+        <span>
+          Last updated <DateTime datestamp={lastUpdated} />
+        </span>
+      )}
+    </div>
+  ) : null;
 
 LastUpdated.propTypes = {
   lastUpdated: PropTypes.instanceOf(Date),
   live: PropTypes.bool,
 };
 LastUpdated.defaultProps = {
-  lastUpdated: new Date(),
+  lastUpdated: false,
   live: true,
 };
 
