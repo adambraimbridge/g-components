6008ada2fcddb33d0aa31012a638014bca767cde
Ændrew Rininsland <andrew.rininsland@ft.com>
2019-12-11 13:20:19 +0000

    Merely existence checks lastUpdated prop now

--- components/last-updated/index.js
+++ components/last-updated/index.js
@@ -8,30 +8,35 @@ import PropTypes from 'prop-types';
 import DateTime from '../datetime';
 import './styles.scss';
 
-const validDate = date => Object.prototype.toString.call(date) === '[object Date]';
-
-const LastUpdated = ({ lastUpdated, live }) =>
-  lastUpdated && validDate(lastUpdated) ? (
-    <div className={`last-updated${live ? ' last-updated--live' : ''}`}>
-      {live ? (
-        <div className="o-teaser o-teaser--small" data-o-component="o-teaser">
-          <div className="o-teaser__content">
-            <div
-              className="o-teaser__timestamp o-teaser__timestamp--inprogress"
-              style={{ display: 'inline-block' }}
-            >
-              <span className="o-teaser__timestamp-prefix" />
-            </div>
-            Last updated <DateTime datestamp={lastUpdated} />
+const LastUpdated = ({ lastUpdated, live }) => (
+  <div className={`last-updated${live ? ' last-updated--live' : ''}`}>
+    {live ? (
+      <div className="o-teaser o-teaser--small" data-o-component="o-teaser">
+        <div className="o-teaser__content">
+          <div
+            className="o-teaser__timestamp o-teaser__timestamp--inprogress"
+            style={{ display: 'inline-block' }}
+          >
+            <span className="o-teaser__timestamp-prefix" />
           </div>
+          {lastUpdated ? (
+            <React.Fragment>
+              Last updated <DateTime datestamp={lastUpdated} />
+            </React.Fragment>
+          ) : (
+            'Live'
+          )}
         </div>
-      ) : (
+      </div>
+    ) : (
+      lastUpdated && (
         <span>
           Last updated <DateTime datestamp={lastUpdated} />
         </span>
-      )}
-    </div>
-  ) : null;
+      )
+    )}
+  </div>
+);
 
 LastUpdated.propTypes = {
   lastUpdated: PropTypes.instanceOf(Date),
