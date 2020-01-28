86555284cde1d6d16cade9ddc9a4b99fa9064e17
Ændrew Rininsland <andrew.rininsland@ft.com>
2020-01-28 18:49:00 +0000

    :heavy_plus_sign: Adds a modal dialog component

---
+++ components/modal/index.js
@@ -0,0 +1,59 @@
+/**
+ * @file
+ * Modal dialog box
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { createPortal } from 'react-dom';
+import { usePortal } from '../../shared/hooks';
+import './styles.scss';
+
+const Modal = ({ className, parent, children, isOpen, closeModal, closeButton }) => {
+  const parentEl = usePortal(parent);
+  return createPortal(
+    isOpen && (
+      <React.Fragment>
+        <div
+          className={classnames('g-modal-background', className)}
+          role="button"
+          aria-label="Close modal"
+          onClick={closeModal}
+          onKeyPress={closeModal}
+          tabIndex={0}
+        />
+        <div className="g-modal">
+          {closeButton && (
+            <div
+              className="g-modal__close-button"
+              role="button"
+              onClick={closeModal}
+              onKeyPress={closeModal}
+              tabIndex={-1}
+            >
+              <i className="g-modal__close-button-icon" />
+            </div>
+          )}
+          {children}
+        </div>
+      </React.Fragment>
+    ),
+    parentEl,
+  );
+};
+
+Modal.propTypes = {
+  children: PropTypes.node,
+  parent: PropTypes.oneOfType([PropTypes.string, PropTypes.string]),
+  closeButton: PropTypes.bool,
+  classNames: PropTypes.string,
+};
+
+Modal.defaultProps = {
+  parent: undefined,
+  closeButton: true,
+  classNames: undefined,
+};
+
+export default Modal;
---
+++ shared/hooks.js
@@ -0,0 +1,62 @@
+/**
+ * @file
+ * Sundry hooks that have multiple use-cases
+ */
+
+import { useRef, useEffect } from 'react';
+import { isElement } from './helpers';
+
+/**
+ * @function
+ * Provides a standardised way of creating the container DOM elements for portals
+ * Usage:
+  ```
+  const SomeComponent = () => {
+    const target = usePortal();
+    return createPortal(<div>my modal</div>, target);
+  }
+  ```
+ *
+ * Modified from: https://www.jayfreestone.com/writing/react-portals-with-hooks/
+ */
+export const usePortal = parent => {
+  const rootRef = useRef(null);
+
+  useEffect(() => {
+    /* eslint-disable no-nested-ternary */
+    const parentEl = isElement(parent)
+      ? parent
+      : typeof parent === 'string'
+      ? document.querySelector(parent) || document.createElement('div')
+      : document.createElement('div');
+    /* eslint-enable no-nested-ternary */
+
+    // Unmounted
+    if (!parentEl.parentElement) {
+      parentEl.classList.add('g-portal-parent');
+      document.body.insertBefore(parentEl, document.body.lastElementChild.nextElementSibling);
+    }
+
+    parentEl.appendChild(rootRef.current);
+
+    return () => {
+      rootRef.current.remove();
+      // Remove parent element if empty.
+      if (parentEl.childNodes.length === 0) parentEl.remove();
+    };
+  }, [parent]);
+
+  return (() => {
+    if (!rootRef.current) {
+      const rootEl = document.createElement('div');
+      rootEl.classList.add('g-portal');
+      rootRef.current = rootEl;
+    }
+
+    return rootRef.current;
+  })();
+};
+
+export default {
+  usePortal,
+};
