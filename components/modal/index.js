10b86be13200c0f8469a3d0a14e5f6db16e9f5e4
Ændrew Rininsland <andrew.rininsland@ft.com>
2020-02-26 13:34:05 +0000

    Committing before throwing everything in the bin. :(

--- components/modal/index.js
+++ components/modal/index.js
@@ -3,24 +3,94 @@
  * Modal dialog box
  */
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { createPortal } from 'react-dom';
 import { usePortal } from '../../shared/hooks';
 import './styles.scss';
 
-const Modal = ({ className, parent, children, isOpen, closeModal, closeButton }) => {
+const Modal = ({ className, parent, children, isOpen, closeModal: closeCallback, closeButton }) => {
   const parentEl = usePortal(parent);
+  const lastFocused = useRef(null);
+  const childContainer = useRef(null);
+  const closeButtonRef = useRef(null);
+  const allFocusable = useRef(null);
+
+  const closeModal = useCallback(
+    (...args) => {
+      if (lastFocused.current) lastFocused.current.focus();
+      allFocusable.current.forEach(el => {
+        el.tabindex = el.originalTabIndex;
+      });
+      closeCallback(...args);
+    },
+    [closeCallback],
+  );
+
+  // Do some fuckery with tabindices
+  useEffect(() => {
+    if (isOpen) {
+      lastFocused.current = document.activeElement;
+      allFocusable.current = document.querySelectorAll(
+        'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])',
+      );
+      allFocusable.current.forEach(el => {
+        el.originalTabIndex = el.tabIndex;
+        el.tabIndex = -1;
+      });
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (!childContainer.current || !closeButtonRef.current) return;
+
+    // Via https://gomakethings.com/how-to-get-the-first-and-last-focusable-elements-in-the-dom/
+    const focusable = [
+      ...childContainer.current.querySelectorAll(
+        'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])',
+      ),
+      closeButtonRef.current,
+    ];
+
+    const [focusableFirstChild] = focusable;
+    // Focus on first focasable
+    focusableFirstChild.focus();
+    const handler = ({ keyCode }) => {
+      if (keyCode === 9) {
+        const currentElIndex = focusable.indexOf(document.activeElement);
+        console.log(currentElIndex);
+        if (currentElIndex === -1 && focusableFirstChild) {
+          console.log('first child!');
+          focusableFirstChild.focus();
+        } else if (currentElIndex + 1 < focusable.length) {
+          console.log('another child!');
+          focusable[currentElIndex + 1].focus();
+        } else if (focusableFirstChild) {
+          console.log('loop');
+          focusableFirstChild.focus();
+        } else {
+          throw new Error('Unhandled focus exception');
+        }
+      }
+    };
+    document.addEventListener('keydown', handler);
+    return () => {
+      document.removeEventListener('keydown', handler);
+    };
+  }, [children, childContainer, closeButtonRef, isOpen]);
 
   // Set up escape key handler
   useEffect(() => {
     const handler = ({ keyCode }) => {
-      if (keyCode === 27) closeModal();
+      if (keyCode === 27) {
+        lastFocused.current.focus();
+        closeModal();
+      }
     };
-    document.addEventListener('onkeydown', handler);
+    document.addEventListener('keydown', handler);
     return () => {
-      document.removeEventListener('onkeydown', handler);
+      document.removeEventListener('keydown', handler);
     };
   }, [closeModal]);
 
@@ -45,12 +115,14 @@ const Modal = ({ className, parent, children, isOpen, closeModal, closeButton })
                   role="button"
                   onClick={closeModal}
                   onKeyPress={closeModal}
-                  tabIndex={0}
+                  ref={closeButtonRef}
                   aria-label="Close dialog"
                 />
               </div>
             )}
-            {children}
+            <div className="g-modal__body" ref={childContainer}>
+              {children}
+            </div>
           </div>
         </React.Fragment>
       ),
