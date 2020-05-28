68621cfc2834f4c4c2c3d70f44dc1743c2ba7607
Max Harlow <max.harlow@ft.com>
2020-05-28 14:48:08 +0100

    Add accordion

---
+++ components/accordion/index.js
@@ -0,0 +1,50 @@
+/**
+ * @file
+ * An accordion component
+ */
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './styles.scss';
+
+const Accordion = ({ children }) => {
+  return (
+    <div className="g-accordion">
+      {children}
+    </div>
+  );
+};
+
+const AccordionSection = ({ isExpanded = false, children: [title, ...body] }) => {
+  const [expanded, setExpanded] = useState(isExpanded);
+  return (
+    <div className="g-accordion-section">
+      <div
+        className={`g-accordion-section__header ${expanded ? 'g-accordion-section__header--expanded' : ''}`}
+        onClick={() => setExpanded(!expanded)}
+      >
+        {title}
+      </div>
+      {expanded && (<div className="g-accordion-section__body">{body}</div>)}
+    </div>
+  );
+};
+
+Accordion.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)]).isRequired,
+};
+
+Accordion.defaultProps = {};
+
+Accordion.displayName = 'GAccordion';
+
+AccordionSection.propTypes = {
+  isExpanded: PropTypes.boolean,
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)]).isRequired,
+};
+
+AccordionSection.defaultProps = {};
+
+AccordionSection.displayName = 'GAccordionSection';
+
+export { Accordion, AccordionSection };
