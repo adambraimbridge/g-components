09f29e879e2180b41cbea98de7e364400136ce52
Max Harlow <max.harlow@ft.com>
2020-05-29 17:20:36 +0100

    Warning: Invalid DOM property charset. Did you mean charSet?

--- components/html-head/index.js
+++ components/html-head/index.js
@@ -50,7 +50,7 @@ const HtmlHead = ({
 
   return (
     <Tag>
-      <meta charset="utf-8" />
+      <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
