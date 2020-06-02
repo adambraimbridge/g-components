961ae2a881adb9d5500dee9993d6c10c41e1f92d
Ændrew Rininsland <andrew.rininsland@ft.com>
2020-06-02 15:59:04 +0100

    Reverting README

--- config-overrides.js
+++ config-overrides.js
@@ -2,10 +2,10 @@ module.exports = {
   
   jest: function (config) {
     config.browser = true;
-    // config.clearMocks = true;
+    config.clearMocks = true;
     config.moduleFileExtensions.push('mdx');
     config.transform = {
-      '\\.txt$': 'jest-raw-loader',
+      // '\\.txt$': 'jest-raw-loader',
       '^.+\\.mdx$': '@storybook/addon-docs/jest-transform-mdx',
        ...config.transform
     }
