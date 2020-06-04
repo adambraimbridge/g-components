625a7eebfb8421a8d9ab64c7c8338b96137793c4
Ændrew Rininsland <andrew.rininsland@ft.com>
2020-06-04 18:42:26 +0100

    Restructuring Layout to be much simpler

--- src/layout/index.js
+++ src/layout/index.js
@@ -5,16 +5,13 @@
 
 import React, { createContext } from 'react';
 import PropTypes from 'prop-types';
-import { strftime } from '../shared/helpers';
 import { flagsPropType, StringBoolPropType } from '../shared/proptypes';
 import Header from '../header';
 import Analytics from '../analytics';
 import { TopAd } from '../ads';
-import ArticleHead from '../article-head';
 import OnwardJourney from '../onwardjourney';
 import Comments from '../comments';
 import Footer from '../footer';
-import { GridChild, GridRow, GridContainer } from '../grid';
 import './styles.scss';
 import { useAds, useLayoutChangeEvents } from '../shared/hooks';
 
@@ -25,7 +22,6 @@ const Article = ({
   ads,
   children,
   defaultContainer,
-  customArticleHead,
   bodyColspan,
   headerColspan,
   ...props
@@ -33,20 +29,12 @@ const Article = ({
   const breakpoint = useLayoutChangeEvents();
   useAds(ads, flags.ads);
 
-  const hasCustomChildren =
-    React.Children.toArray(children).some(
-      el => (el.className || '').includes('o-grid-container') || el.type === GridContainer,
-    ) || !defaultContainer;
-
-  const articleHeadComponent = customArticleHead || <ArticleHead {...props} flags={flags} />;
-
   return (
     <Context.Provider
       value={{
         flags,
         ads,
         defaultContainer,
-        customArticleHead,
         breakpoint,
         ...props,
       }}
@@ -54,68 +42,7 @@ const Article = ({
       {flags.analytics && <Analytics {...{ ...props, flags, breakpoint }} />}
       {flags.ads && <TopAd />}
       {flags.header && <Header key="header" {...{ ...props, flags, breakpoint }} />}
-      <main key="main" role="main">
-        <article className="article" itemScope itemType="http://schema.org/Article">
-          <GridContainer className="article-head">
-            <GridRow>
-              <GridChild colspan="12 S11 Scenter M9 L8 XL7">{articleHeadComponent}</GridChild>
-            </GridRow>
-          </GridContainer>
-          <div className="article-body o-editorial-typography-body" itemProp="articleBody">
-            {hasCustomChildren ? (
-              React.Children.map(children, child =>
-                React.cloneElement(
-                  child,
-                  typeof !child.type || child.type === 'string' ? {} : { ...props, breakpoint },
-                ),
-              )
-            ) : (
-              <GridContainer>
-                <GridRow>
-                  <GridChild colspan={bodyColspan}>
-                    <div className="o-editorial-layout-wrapper">
-                      {React.Children.map(children, child =>
-                        React.cloneElement(
-                          child,
-                          !child.type || typeof child.type === 'string'
-                            ? {}
-                            : { ...props, breakpoint },
-                        ),
-                      )}
-                    </div>
-                  </GridChild>
-                </GridRow>
-              </GridContainer>
-            )}
-          </div>
-
-          <footer
-            className="o-typography-footer"
-            itemProp="publisher"
-            itemScope
-            itemType="https://schema.org/Organization"
-          >
-            <GridContainer>
-              <GridRow>
-                <GridChild colspan="12 S11 Scenter M9 L8 XL7">
-                  <small>
-                    <a
-                      href="http://www.ft.com/servicestools/help/copyright"
-                      data-trackable="link-copyright"
-                    >
-                      Copyright
-                    </a>{' '}
-                    <span itemProp="name">The Financial Times</span> Limited{' '}
-                    {strftime('%Y')(new Date())}. All rights reserved. You may share using our
-                    article tools. Please don&apos;t cut articles from FT.com and redistribute by
-                    email or post to the web.
-                  </small>
-                </GridChild>
-              </GridRow>
-            </GridContainer>
-          </footer>
-        </article>
-      </main>
+      {children}
       {flags.onwardjourney && <OnwardJourney key="oj" {...{ ...props, breakpoint }} />}
       {flags.comments && <Comments key="comments" {...{ ...props, flags, breakpoint }} />}
       {flags.footer && <Footer key="footer" {...{ ...props, flags, breakpoint }} />}
@@ -135,7 +62,6 @@ Article.propTypes = {
   flags: flagsPropType.isRequired,
   children: PropTypes.node,
   defaultContainer: PropTypes.bool,
-  customArticleHead: PropTypes.node,
   wrapArticleHead: PropTypes.bool,
   bodyColspan: PropTypes.string,
   headerColspan: PropTypes.string,
@@ -150,9 +76,7 @@ Article.defaultProps = {
   },
   children: null,
   defaultContainer: true,
-  customArticleHead: null,
   wrapArticleHead: true,
-  bodyColspan: '12 S11 Scenter M9 L8 XL7',
   headerColspan: '12 S11 Scenter M9 L8 XL7',
 };
 
